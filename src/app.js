4f10a4ca4c561a11374617de2816107e468f8c7d
Grandon Smith <grandon.smith@yahoo.com>
2020-12-09 14:54:14 -0500

    finished modularizing list data

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ const { NODE_ENV } = require('./config');
 const { json } = require('express');
 const { v4: uuid } = require('uuid');
 const cardRouter = require('./card/card-router');
+const listRouter = require('./list/list-router')
 const bodyParser = express.json();
 
 const app = express();
-
 
   const lists = [{
     id: 1,
@@ -26,6 +26,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 app.use(cardRouter)
+app.use(listRouter)
 app.use(express.json())
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
@@ -41,82 +42,6 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
-});
-
-app.get('/list', (req, res) => {
-    res.json(lists)
-});
-
-app.get('/list/:id', (req, res) => {
-    const { id } = req.params;
-    const list = list.find(li => li.id == id);
-    if(!list) {
-        logger.error(`List with id ${id} wasn't found!`);
-        return res.status(404).send('List not found')
-    }
-    return res.json(list);
-});
-
-app.post('/list', (req, res) => {
-    const { header, cardIds = [] } = req.body;
-  
-    if (!header) {
-      logger.error(`Header is required`);
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
-    // check card IDs
-    if (cardIds.length > 0) {
-      let valid = true;
-      cardIds.forEach(cid => {
-        const card = cards.find(c => c.id == cid);
-        if (!card) {
-          logger.error(`Card with id ${cid} not found in cards array.`);
-          valid = false;
-        }
-      });
-  
-      if (!valid) {
-        return res
-          .status(400)
-          .send('Invalid data');
-      }
-    }
-    // get an id
-    const id = uuid();
-    const list = {
-      id,
-      header,
-      cardIds
-    };
-  
-    lists.push(list);
-    logger.info(`List with id ${id} created`);
-    res
-      .status(201)
-      .location(`http://localhost:8000/list/${id}`)
-      .json({id});
-  });
-
-app.delete('/list/:id', (req, res) => {
-    const { id } = req.params;
-
-    const listIndex = lists.findIndex(li => li.id == id);
-
-    if (listIndex === -1) {
-        logger.error(`List with id ${id} not found.`);
-        return res
-        .status(404)
-        .send('Not Found');
-    }
-
-    lists.splice(listIndex, 1);
-
-    logger.info(`List with id ${id} deleted.`);
-    res
-        .status(204)
-        .end();
 });
 
 app.use(function errorHandler(error, req, res, next) {
